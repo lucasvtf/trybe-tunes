eebd1a827f592f7dc8de3569e0decf66e74600e9
Lucas Victor <lucassvtf@gmail.com>
2022-08-30 17:38:10 -0300

    Requisito 3

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import Loading from './Loading';
+import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
+  state = {
+    user: '',
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.fecthUserAPI();
+  }
+
+  fecthUserAPI = async () => {
+    const data = await getUser();
+    this.setState({ user: data.name });
+    this.setState({ loading: false });
+  };
+
   render() {
+    const { user, loading } = this.state;
     return (
       <header data-testid="header-component">
         <h1>TrybeTunes</h1>
-        <h3>Nome do Usuário</h3>
+        {loading && <Loading />}
+        <h3
+          name="userName"
+          data-testid="header-user-name"
+        >
+          {user}
+
+        </h3>
       </header>
     );
   }
