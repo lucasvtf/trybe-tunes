bde0d788447886306c4f632537a2623f9f6da43e
Lucas Victor <lucassvtf@gmail.com>
2022-08-30 15:39:59 -0300

    Requisito 1

---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        <p>e</p>
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
